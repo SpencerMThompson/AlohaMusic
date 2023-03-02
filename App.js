bd467564eb695a51fdc495728780526ab364b143
Spencer Thompson <st746866@southeast.edu>
2023-03-02 16:07:03 -0500

    init

--- App.js
+++ App.js
@@ -1,20 +1,138 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import {Audio, interruptionModeAndroid, interruptionModeIOS} from 'expo-av';
+import { Feather } from '@expo/vector-icons';
+import * as SplashScreen from 'expo-splash-screen';
+import {
+  StyleSheet,
+  Text,
+  Image,
+  TouchableOpacity,
+  View
+} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+SplashScreen.preventAutoHideAsync();
+setTimeout(SplashScreen.hideAsync, 2000);
+
+const art = require("./images/ukulele.png");
+
+export default class App extends Component {
+  state = {
+    isPlaying: false,
+    playbackInstance: null,
+    volume: 1.0,
+    currentTrackIndex: 0,
+    isBuffering: false,
+  }
+
+	async componentDidMount() {
+    await Audio.setAudioModeAsync({
+      allowsRecordingIOS: false,
+      playThroughEarpieceAndroid: true,
+      interruptionModeIOS: InterruptionModeIOS.DoNotMix,
+      playsInSilentModeIOS: true,
+      shouldDuckAndroid: true,
+      interruptionModeAndroid: InterruptionModeAndroid.DoNotMix,
+    });
+    this.loadAudio();
+  }
+
+  handlePlayPause = async () => {
+    const { isPlaying, playbackInstance } = this.state;
+    isPlaying ? await playbackInstance.pauseAsync() : await playbackInstance.playAsync();
+    this.setState({
+      isPlaying: !isPlaying
+    });
+  }
+
+  onPlaybackStatusUpdate = (status) => {
+    this.setState({
+      isBuffering: status.isBuffering
+    });
+  }
+
+  async loadAudio() {
+    const playbackInstance = new Audio.Sound();
+    const source = './music/ukulele.mp3'
+		const status = {
+			shouldPlay: this.state.isPlaying,
+			volume: this.state.volume,
+    };
+    playbackInstance
+      .setOnPlaybackStatusUpdate(
+        this.onPlaybackStatusUpdate
+      );
+    await playbackInstance.loadAsync(source, status, false);
+    this.setState({
+      playbackInstance
+    });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.header}>
+        <Text style={[styles.largeText, styles.buffer]}>
+          Aloha Music
+        </Text>
+        </View>
+        <Image style={styles.image} source={art}></Image>
+        <View style={styles.controls}>
+          <TouchableOpacity
+            style={styles.control}
+            onPress={this.handlePlayPause}>
+            {this.state.isPlaying ?
+              <Feather name="pause" size={32} style={styles.buffer}/> :
+              <Feather name="play" size={32} style={styles.buffer}/>
+            }
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f4e3cf',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  trackInfo: {
+    padding: 40,
+    backgroundColor: '#191A1A',
+  },
+  header:{
+    backgroundColor: '#da9547',
+    width: 300,
+    height: 40,
+    marginBottom: 30,
+  },
+  buffer: {
+    color: '#563822',
+  },
+  trackInfoText: {
+    textAlign: 'center',
+    flexWrap: 'wrap',
+  },
+  largeText: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: 'black',
+    alignContent: 'center',
+    textAlign: 'center',
+  },
+  smallText: {
+    fontSize: 16
+  },
+  control: {
+    margin: 20
+  },
+  controls: {
+    flexDirection: 'row'
+  },
+  image: {
+    height: 500,
+    width: 300,
   },
 });
